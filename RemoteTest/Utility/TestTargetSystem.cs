e7a78ac46cffc33a2904bfbdad319f765b4f8d46
Anuj003-IGT <anuj.dadwal@philips.com>
2025-07-30 09:59:31 +0530

    Update TestTargetSystem.cs

--- RemoteTest/Utility/TestTargetSystem.cs
+++ RemoteTest/Utility/TestTargetSystem.cs
@@ -1,10 +1,4 @@
-﻿#region (C) Copyright 2023 Philips Medical Systems Nederland B.V.
-//
-// All rights are reserved. Reproduction or transmission in whole or in part, in
-// any form or by any means, electronic, mechanical or otherwise, is prohibited
-// without the prior written permission of the copyright owner.
-//
-#endregion
+
 
 namespace Utility
 {
